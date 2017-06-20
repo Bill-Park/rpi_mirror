7941cef2c77f0bfc1644b9587b0df37f1b6f5881
Bill-Park <tkddn0916@gmail.com>
2017-06-21 00:09:11 +0900

    :sparkles: add lowest, highest temp

--- source/get_api.py
+++ source/get_api.py
@@ -4,7 +4,22 @@ import pytz
 import datetime
 import json
 
-
+'''
+POP 강수확률 %-1 %
+PTY 강수형태 코드값-1
+R06 6시간 강수량범주 (1 mm)-1 mm
+REH 습도 %-1 %
+S06 6시간 신적설범주(1 cm)-1 cm
+SKY 하늘상태 코드값 -1
+T3H 3시간 기온 ℃-50 ℃
+TMN 아침 최저기온 ℃-50 ℃
+TMX 낮 최고기온 ℃-50 ℃
+UUU 풍속(동서성분) m/s-100 m/s
+VVV 풍속(남북성분) m/s100 m/s
+WAV 파고 M-1 m
+VEC 풍향 m/s-1
+WSD 풍속1-1
+'''
 
 #['0200', '0500', '0800', '1100', '1400', '1700', '2000', '2300']
 standard_time = [2, 5, 8, 11, 14, 17, 20, 23]
@@ -38,6 +53,7 @@ def get_weather_data() :
     type = "&_type=json"
 
     api_url = url + key + date + time + nx + ny + numOfRows + type
+    print(api_url)
 
     data = urllib.request.urlopen(api_url).read().decode('utf8')
     parsed_json = json.loads(data)['response']['body']['items']['item']
@@ -49,6 +65,11 @@ def get_weather_data() :
     for one_parsed in parsed_json :
         if one_parsed['fcstDate'] == target_date and one_parsed['fcstTime'] == target_time :
             print(one_parsed['category'], one_parsed['fcstValue'])
+
+        if one_parsed['fcstDate'] == target_date and one_parsed['category'] == 'TMX':
+            print('highest temp : ' + str(one_parsed['fcstValue']))
+        elif one_parsed['fcstDate'] == target_date and one_parsed['category'] == 'TMN':
+            print('lowest temp : ' + str(one_parsed['fcstValue']))
 
 
 
